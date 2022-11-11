dcbc1abc97420a0b013e18778297b0dc0d0ff6b6
dev_josh <Alanajoshua535@gmail.com>
2022-11-11 15:34:10 +0100

    hello world

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: A script for hello world 
+#Version: 1.0
+#Created Date:  Fri Nov 11 03:32:50 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+echo "Hello world"
+# END #
