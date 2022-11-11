5757494f0d4cb0a0add8746f021632957ffb3d86
dev_josh <Alanajoshua535@gmail.com>
2022-11-11 16:44:36 +0100

    count args

--- counting_special.sh
+++ counting_special.sh
@@ -5,5 +5,9 @@
 #Modified Date: 
 #Author: Alana Joshua
 # START #
-echo "Your current given paramters are $#"
+#echo "Your current given paramters are $#"
+
+if [ $# -lt 1 ] ; then
+    echo "Program Usage is './scriptname.sh' options"
+fi
 # END #
