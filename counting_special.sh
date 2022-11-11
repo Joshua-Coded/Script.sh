5f1f810dbd7f6f6814b34b0e8bd0470e7c29ade7
dev_josh <Alanajoshua535@gmail.com>
2022-11-11 16:25:44 +0100

    countargs

---
+++ counting_special.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: Counting given positional parameters.
+#Version: 1.0
+#Created Date:  Fri Nov 11 04:22:54 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+echo "Your current given paramters are $#"
+# END #
