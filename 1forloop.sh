7a72c27b60e2406565d23e98a15d6a891eea9ca8
dev_josh <Alanajoshua535@gmail.com>
2022-11-13 14:40:40 +0100

    1forloop.sh

---
+++ 1forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose: one more example
+#Version: 1.0
+#Created Date:  Sun Nov 13 02:38:56 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+for i in 1 2 3 4 5 5
+do
+    echo "$i"
+    done
+# END #
