f42054f6fd18ae26d8fe1e1df136e8a28ed7cb5d
dev_josh <Alanajoshua535@gmail.com>
2022-11-15 16:59:04 +0100

    add.sh

---
+++ add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Purpose: Addition script 
+#Version: 1.0
+#Created Date:  Tue Nov 15 04:54:54 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+
+echo "Addition of X+Y"
+echo -e "Enter the value of X: \c"
+read -r X
+echo  -e "Enter the value of Y: \c"
+read -r Y
+echo "X+Y = $X+$Y = $[ X+Y ]"
+echo "thank you for entering"
+# END #
