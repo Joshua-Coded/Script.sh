08e3d0da972ac6de40c5a3d8a0231435e4865913
dev_josh <Alanajoshua535@gmail.com>
2022-11-15 17:12:56 +0100

    info.sh

---
+++ info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Purpose: eval command evaluating twice 
+#Version: 1.0
+#Created Date:  Tue Nov 15 05:04:23 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+
+echo "Hi you there"
+echo "what is your name? (type your name here and press enter)"
+read -r NM
+echo "Hi $NM Good Evening"
+echo "you are currently logged in as $USERNAME"
+echo "YOur present working directory is `pwd`"
+# END #
