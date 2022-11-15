b859183117708c6891647b94c6de852397c1525b
dev_josh <Alanajoshua535@gmail.com>
2022-11-15 17:28:45 +0100

    ud.sh

---
+++ ud.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Purpose: eval command  
+#Version: 1.0
+#Created Date:  Tue Nov 15 05:25:05 PM WAT 2022
+#Modified Date: 
+#Author: Alana Joshua
+# START #
+
+echo "WELCOME TO $USER"
+echo "Your present working directory is `pwd`"
+echo "present processes are `ps -a`"
+echo "Now time is `date`"
+
+# END #
